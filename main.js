3556ff15a7b9082b60dda256200f4f5243532ff9
Ivan Chen <tachen2@cisco.com>
2021-11-08 10:34:24 +0800

    chore(template): Template for express

---
+++ main.js
@@ -0,0 +1,24 @@
+const express = require('express'),
+    app = new express(),
+    livereload = require('livereload'),
+    connectLivereload = require('connect-livereload')
+
+const liveReloadServer = livereload.createServer();
+liveReloadServer.server.once("connection", () => {
+    setTimeout(() => {
+        liveReloadServer.refresh("/");
+    }, 100);
+});
+app.use(connectLivereload())
+
+app.set('view engine', 'ejs')
+app.use(require('express-ejs-layouts'))
+app.use(express.static('public'))
+
+app.set('port', process.env.PORT || 3000)
+app.listen(app.get('port'), () => {
+    console.log(`Server is running at http://localhost:${app.get('port')}`)
+})
+
+const homeController = require('./controllers')
+app.get('/', homeController.index)
